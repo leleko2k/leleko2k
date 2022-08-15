9ad2b8eeb966fce4928fac496ad497e3010b85ad
leleko2k <81333567+leleko2k@users.noreply.github.com>
2022-08-15 04:51:11 +0300

    Create Useful_Soft.sh

---
+++ Useful_Soft.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Installing required soft"
+sudo apt-get install curl -y
+sudo apt-get install wget -y
+sudo apt-get install screen -y
+sudo apt-get install net-tools -y
+sudo apt-get install git -y
+sudo apt-get install ncdu htop build-essential libssl-dev gcc make libssl-dev pkg-config npm -y
+echo "Required Soft Installed Successfully"
