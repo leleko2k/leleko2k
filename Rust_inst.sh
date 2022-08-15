5d817a286f4c7ed5585d2c097d080935e4cc2016
leleko2k <81333567+leleko2k@users.noreply.github.com>
2022-08-16 01:26:57 +0300

    Update Rust_inst.sh

--- Rust_inst.sh
+++ Rust_inst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Start Rust Installation"
 curl https://sh.rustup.rs -sSf | bash
-echo "1"
+echo "1" >> bash
 source $HOME/.cargo/env
 rustc --version
 echo "Rust Installed Successfully"
