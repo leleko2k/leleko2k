92841407c036ea7c3b10ee423470fce904d663df
leleko2k <81333567+leleko2k@users.noreply.github.com>
2022-08-15 12:14:35 +0300

    Update Useful_Soft.sh

--- Useful_Soft.sh
+++ Useful_Soft.sh
@@ -5,4 +5,6 @@ sudo apt-get install wget -y
 sudo apt-get install screen -y
 sudo apt-get install net-tools -y
 sudo apt-get install git -y
+sudo apt-get install ncdu -y
+sudo apt-get install htop -y
 echo "Required Soft Installed Successfully"
